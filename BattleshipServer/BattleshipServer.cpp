1bba2d1bbafee9b39a0a1490f5b4661eeea3df25
Agere <bmateusz1998@gmail.com>
2019-12-23 16:07:54 +0100

    Basic echo server

--- BattleshipServer/BattleshipServer.cpp
+++ BattleshipServer/BattleshipServer.cpp
@@ -1,9 +1,37 @@
 ﻿#include "BattleshipServer.h"
 
-using namespace std;
 
-int main()
-{
-	cout << "Battleship Server" << endl;
-	return 0;
+#define BACKLOG 10
+#define BUF_SIZE 255
+
+int main() {
+    int sd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sd == -1)perror("Socket error: ");
+
+    struct sockaddr_in sa;
+    memset(&sa, 0, sizeof(struct sockaddr_in));
+    sa.sin_family = AF_INET;
+    sa.sin_port = htons(2345);
+    sa.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if (bind(sd, (struct sockaddr*) & sa, sizeof(sa)) == -1)perror("Bind error: ");
+    listen(sd, BACKLOG);
+    printf("Listening\n");
+
+    while (1) {
+        int cd = accept(sd, NULL, NULL);
+        printf("Connected\n");
+
+        char buf[BUF_SIZE];
+        int l = read(cd, buf, BUF_SIZE - 1);
+        buf[l] = '\0';
+
+        printf("%s\n", buf);
+
+        write(cd, buf, l);
+
+        close(cd);
+    }
+
+    close(sd);
 }
--- BattleshipServer/BattleshipServer.h
+++ BattleshipServer/BattleshipServer.h
@@ -1,8 +1,8 @@
-﻿// BattleshipServer.h: plik dołączany dla standardowych systemowych plików dołączanych,
-// lub pliki dołączane specyficzne dla projektu.
+﻿#pragma once
 
-#pragma once
-
-#include <iostream>
-
-// TODO: W tym miejscu przywołaj dodatkowe nagłówki wymagane przez program.
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
