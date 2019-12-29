8e83d1a3015f6dd5693483845af37b3634c8c45c
Agere <bmateusz1998@gmail.com>
2019-12-29 16:02:39 +0100

    Rewrite main function

--- BattleshipServer/BattleshipServer.cpp
+++ BattleshipServer/BattleshipServer.cpp
@@ -1,16 +1,39 @@
-﻿#include <BattleshipServer.h>
+﻿#include <ConnectionManager.h>
+#include <SocketBSD.h>
+#include <iostream>
+#include <pthread.h>
 
 #define SERVER_PORT 2680
 
+using namespace std;
 using namespace Sockets;
 using namespace BattleshipServer;
 
-int main() {
+void* runServer(void* serverSocket) {
+	Socket* server = (Socket*)serverSocket;
 	ConnectionManager connectionManager;
-	SocketBSD* server = new SocketBSD("", SERVER_PORT);
-	server->listen();
 	while (true) {
-		connectionManager.handleConnection(server->accept());
+		try {
+			connectionManager.handleConnection(server->accept());
+		}
+		catch (std::runtime_error & exception) {
+			cout << exception.what() << endl;
+		}
 	}
-	server->close();
+}
+
+int main() {
+	SocketBSD server("", SERVER_PORT);
+	server.listen();
+	pthread_t serverThread;
+	pthread_create(&serverThread, NULL, runServer, (void*)&server);
+	cout << "The server is running on port " << SERVER_PORT << ", type 'stop' to stop the server" << endl;
+	string s;
+	while (getline(cin, s)) {
+		if (s == "stop") {
+			break;
+		}
+	}
+	server.close();
+	cout << "The server has been stopped correctly" << endl;
 }
--- BattleshipServer/BattleshipServer.h
+++
@@ -1,4 +0,0 @@
-﻿#pragma once
-
-#include <ConnectionManager.h>
-#include <SocketBSD.h>
