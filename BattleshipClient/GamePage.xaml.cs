af7f6f73193632f0b2568db95901a5dfa3cc8e61
Agere <bmateusz1998@gmail.com>
2019-12-31 18:05:23 +0100

    Add game page

---
+++ BattleshipClient/GamePage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Navigation;
+
+namespace BattleshipClient {
+
+    public enum GameState { Default, InProgress, Completed, Cancelled }
+
+    /// <summary>
+    /// Interaction logic for GamePage.xaml
+    /// </summary>
+    public partial class GamePage : PageFunction<GameState> {
+
+        public GamePage() {
+            InitializeComponent();
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e) {
+            OnReturn(new ReturnEventArgs<GameState>(GameState.Completed));
+        }
+    }
+}
