7e8e87c48f76928c2bc86604dfb40096e380600f
Agere <bmateusz1998@gmail.com>
2019-12-31 02:32:52 +0100

    Add client connection logic

---
+++ BattleshipClient/GameClient.cs
@@ -0,0 +1,69 @@
+﻿using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace BattleshipClient {
+
+    /// <summary>
+    /// Client-side communication logic
+    /// </summary>
+    class GameClient {
+
+        const int serverPort = 2680;
+
+        private Socket client;
+        private NetworkStream networkStream;
+        private StreamReader reader;
+        private StreamWriter writer;
+
+        /// <summary>
+        /// Resolves a hostname and establishes a connection to a remote host
+        /// </summary>
+        /// <param name="hostname">The host name or IP address to connect to</param>
+        /// <returns></returns>
+        public async Task ConnectAsync(string hostname) {
+            var host = await Task.Run(() => {
+                return Dns.GetHostEntry(hostname);
+            });
+            var endPoint = new IPEndPoint(host.AddressList[0], serverPort);
+            client = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            await client.ConnectAsync(endPoint);
+            networkStream = new NetworkStream(client);
+            reader = new StreamReader(networkStream);
+            writer = new StreamWriter(networkStream);
+            writer.AutoFlush = true;
+        }
+
+        /// <summary>
+        /// Closes the client connection
+        /// </summary>
+        /// <returns></returns>
+        public Task DisconnectAsync() {
+            return Task.Run(() => {
+                reader.Close();
+                writer.Close();
+                networkStream.Close();
+                client.Shutdown(SocketShutdown.Both);
+                client.Close();
+            });
+        }
+
+        /// <summary>
+        /// Reads a line from client socket
+        /// </summary>
+        /// <returns>A string containing a single line received from a remote host</returns>
+        public Task<string> ReadAsync() {
+            return reader.ReadLineAsync();
+        }
+
+        /// <summary>
+        /// Writes a string to client socket, followed by a line delimeter
+        /// </summary>
+        /// <param name="message">A message string to send</param>
+        /// <returns></returns>
+        public Task WriteAsync(string message) {
+            return writer.WriteLineAsync(message);
+        }
+    }
+}
