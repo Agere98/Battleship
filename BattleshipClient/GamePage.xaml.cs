d38c1d6bfc5d948f99c2acf689afbd4bcf3527b1
Agere <bmateusz1998@gmail.com>
2020-01-02 00:32:17 +0100

    Implement game page functionality

--- BattleshipClient/GamePage.xaml.cs
+++ BattleshipClient/GamePage.xaml.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Net.Sockets;
+﻿using System.Collections.Generic;
+using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
 using System.Windows.Navigation;
 
 namespace BattleshipClient {
@@ -15,16 +13,111 @@ namespace BattleshipClient {
     public partial class GamePage : PageFunction<GameState> {
 
         private readonly GameClient client;
+        private readonly PlayerBoard playerBoard;
+        private readonly OpponentBoard opponentBoard;
+        private GameState state;
+        private (int Row, int Column) selectedTile;
 
         public GamePage(GameClient client) {
             InitializeComponent();
             this.client = client;
-            PlayerBoard.Content = new Board(10, 10).GetGrid();
-            OpponentBard.Content = new Board(10, 10).GetGrid();
+            client.MessageReceived += Client_MessageReceived;
+            playerBoard = new PlayerBoard(10, 10, new List<int> { 5, 4, 3, 3, 2 });
+            playerBoard.ShipsPlaced += PlayerBoard_ShipsPlaced;
+            playerBoard.IsActive = true;
+            PlayerBoard.Content = playerBoard.GetGrid();
+            opponentBoard = new OpponentBoard(10, 10);
+            opponentBoard.TileSelected += OpponentBoard_TileSelected;
+            OpponentBard.Content = opponentBoard.GetGrid();
+            state = GameState.InProgress;
+        }
+
+        private void Client_MessageReceived(object sender, GameClient.MessageReceivedEventArgs e) {
+            ParseServerMessage(e.Message);
+        }
+
+        private async void OpponentBoard_TileSelected(object sender, OpponentBoard.TileSelectedEventArgs e) {
+            selectedTile = (e.Row, e.Column);
+            GameStatusLabel.Content = "Waiting for opponent";
+            opponentBoard.IsActive = false;
+            await client.WriteAsync($"fire {e.Column} {e.Row}");
+        }
+
+        private async void PlayerBoard_ShipsPlaced(object sender, PlayerBoard.ShipsPlacedEventArgs e) {
+            GameStatusLabel.Content = "Waiting for opponent";
+            var ships = e.Ships;
+            StringBuilder command = new StringBuilder("ships ");
+            foreach (var ship in ships) {
+                command.Append(ship.Column).Append(" ");
+                command.Append(ship.Row).Append(" ");
+                var orientation = (ship.Orientation == Ship.ShipOrientation.Horizontal) ? "h" : "v";
+                command.Append(orientation).Append(" ");
+            }
+            await client.WriteAsync(command.ToString());
         }
 
         private void LeaveButton_Click(object sender, RoutedEventArgs e) {
-            OnReturn(new ReturnEventArgs<GameState>(GameState.Completed));
+            client.WriteAsync("leave");
+            Close();
+            if (state == GameState.InProgress) {
+                state = GameState.Cancelled;
+            }
+            OnReturn(new ReturnEventArgs<GameState>(state));
+        }
+
+        private void ParseServerMessage(string message) {
+            if (message == "turn") {
+                GameStatusLabel.Content = "Your turn";
+                opponentBoard.IsActive = true;
+            }
+            if (message == "miss") {
+                opponentBoard.SetTile(selectedTile.Row, selectedTile.Column, Board.Tile.Miss);
+            }
+            if (message == "hit") {
+                opponentBoard.SetTile(selectedTile.Row, selectedTile.Column, Board.Tile.Hit);
+            }
+            if (message == "sunk") {
+                opponentBoard.SetTile(selectedTile.Row, selectedTile.Column, Board.Tile.Hit);
+            }
+            if (message == "win") {
+                GameStatusLabel.Content = "Victory!";
+                state = GameState.Completed;
+            }
+            if (message == "lose") {
+                GameStatusLabel.Content = "Defeat";
+                state = GameState.Completed;
+            }
+            if (message == "draw") {
+                GameStatusLabel.Content = "Draw";
+                state = GameState.Completed;
+            }
+            if (message == "opponent left") {
+                GameStatusLabel.Content = "Opponent left";
+                state = GameState.Cancelled;
+            }
+            if (message.StartsWith("fire ")) {
+                var command = message.Split(' ');
+                if (command.Length < 3) return;
+                if (!int.TryParse(command[1], out int column)) return;
+                if (!int.TryParse(command[2], out int row)) return;
+                EnemyFire(row, column);
+            }
+        }
+
+        private void EnemyFire(int row, int column) {
+            var tile = playerBoard.GetTile(row, column);
+            if (tile == Board.Tile.Empty) {
+                playerBoard.SetTile(row, column, Board.Tile.Miss);
+            }
+            if (tile == Board.Tile.Ship) {
+                playerBoard.SetTile(row, column, Board.Tile.Hit);
+            }
+        }
+
+        private void Close() {
+            playerBoard.ShipsPlaced -= PlayerBoard_ShipsPlaced;
+            opponentBoard.TileSelected -= OpponentBoard_TileSelected;
+            client.MessageReceived -= Client_MessageReceived;
         }
     }
 }
